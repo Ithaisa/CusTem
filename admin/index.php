6652800734a3140411b10f08282eb12848568e66
SesarMS <mirandasamanamudcesar@gmail.com>
2023-11-02 13:18:56 +0000

    admin/index;

--- admin/index.php
+++ admin/index.php
@@ -1,13 +1,17 @@
 
 <?php
-    $resultado=$_GET['resultado'] ?? null;
+    $resultado=$_GET['resultado']??null;
     require '../includes/funciones.php';
     require '../includes/config/database.php';
+    session_start();
+    if(!isset($_SESSION['id'])){
+       header('Location: /admin/login.php');
+    }
     incluirTemplate('header');
 
     $db=conectarBD();
-    $query = "";
-    //$datos= mysqli_query($db, $query);
+    $query = "SELECT * FROM productos;";
+    $datos= mysqli_query($db, $query);
 ?>
 
 <link rel="stylesheet" href="index.css">
@@ -16,7 +20,7 @@
     <?php if (intval($resultado)===1){ ?>
         <p class="alerta exito">Anuncio creado correctamente</p>
     <?php } ?>
-    <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+    <a href="/admin/propiedades/crear.php" class="boton boton-verde">Añadir producto</a>
     <table>
         <!-- Fila 1 -->
         <tr>
@@ -26,23 +30,19 @@
             <td>Precio</td>
             <td>Operaciones</td>
         </tr>
-        <?php {//while($fila = mysqli_fetch_assoc($datos)){?>
-            <tr>
-                <td> <img src="/build/img/<?php echo $fila['imagen']?>"> </td>
-                <!-- <td> <?php echo $fila['IDProducto']?></td>
+        <?php while($fila = mysqli_fetch_assoc($datos)){?>
+            <tr>                
+                <td> <img src="../imagenes/<?php echo $fila['Imagen']?>"> </td>
+                <td> <?php echo $fila['IDProducto']?></td>
                 <td> <?php echo $fila['Nombre']?> </td>
-                <td> <?php echo $fila['Imagen']?> </td>
-                <td> <?php echo $fila['Descripcion']?> </td> -->
-                
-
-                <td class="operaciones">
-                    <a href="/admin/propiedades/actualizar.php" class="boton boton-verde">Actualizar propiedad</a>
-                    <button class="boton boton-verde"
-                    onclick="borrarPropiedad(<?php echo $fila['id'];?>)">Borrar propiedad</button>
+                <td> <?php echo $fila['Descripcion']?> </td>
+                <td class="operaciones"> 
+                    <a href="/admin/propiedades/actualizar.php/?id=<?php echo $fila['IDProducto'];?>">Actualizar propiedad</a>
+                    <a class="boton boton-verde" href="/admin/propiedades/borrar.php/?id=<?php echo $fila['IDProducto'];?>">Borrar propiedad</button>
                 </td>
             </tr>
             <?php }?>
             
         </table>    
     </main>
-    <script src="/build/propiedades/borrar.js"></script>
+    <!-- <script src="/build/propiedades/borrar.js"></script> -->
