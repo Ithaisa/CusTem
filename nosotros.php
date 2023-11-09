73724a15f149d5738476a87e21a00841f0e9fe94
Ithaisa <116368834+Ithaisa@users.noreply.github.com>
2023-11-09 21:47:20 +0000

    Create nosotros.php

---
+++ nosotros.php
@@ -0,0 +1,19 @@
+<?php 
+    require './includes/funciones.php';
+    incluirTemplate('header');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/public/css/style.css">
+    <title>Sobre Nosotros</title>
+</head>
+<body>
+    <h1>Sobre nosotros</h1>
+    <script src="https://kit.fontawesome.com/e5d388d5d6.js" crossorigin="anonymous"></script>
+    <script src="/public/js/carrito.js"></script>
+</body>
+</html>
